ea2edff06d1cccfc4d97ccdd3147c58a47f6a2ca
mort5554 <kuniewiczigor@gmail.com>
2024-07-23 13:52:35 +0200

    Add FAQs

--- src/components/FAQsSection.vue
+++ src/components/FAQsSection.vue
@@ -18,37 +18,100 @@ const isOpened5 = ref(false)
       </h2>
       <button class="buttonContact">Contact</button>
     </div>
+
+    <!--Right Side-->
+
     <div class="FAQsSectionRight">
-      <div class="FAQsElement center">
-        <h2 class="FAQsElementHeader">What is your return policy?</h2>
-        <div class="FAQsElementOpen" @click="isOpened1 = !isOpened1" :class="{ rotate: isOpened1 }">
-          +
+      <!--First Element-->
+      <div class="FAQsElement" :class="{ smoothlyOpen: isOpened1 }">
+        <div class="FAQsElementTop">
+          <h2 class="FAQsElementHeader">What is your return policy?</h2>
+          <div
+            class="FAQsElementOpenButton"
+            @click="isOpened1 = !isOpened1"
+            :class="{ rotate: isOpened1 }"
+          >
+            +
+          </div>
+        </div>
+        <div class="FAQsElementClosed" :class="{ FAQsElementOpened: isOpened1 }">
+          We want you to be completely satisfied with your Electrohut purchase. If you're not,
+          simply return your item(s) within 30 days of receipt in new, unused condition with the
+          original packaging for a full refund or exchange. Please visit the ‘Orders’ section on
+          your account page, to initiate the process and for detailed instructions.
         </div>
       </div>
-      <div class="FAQsElement center">
-        <h2 class="FAQsElementHeader">What payment methods do you accept?</h2>
-        <div class="FAQsElementOpen" @click="isOpened2 = !isOpened2" :class="{ rotate: isOpened2 }">
-          +
+      <!--Second Element-->
+      <div class="FAQsElement">
+        <div class="FAQsElementTop">
+          <h2 class="FAQsElementHeader">What payment methods do you accept?</h2>
+          <div
+            class="FAQsElementOpenButton"
+            @click="isOpened2 = !isOpened2"
+            :class="{ rotate: isOpened2 }"
+          >
+            +
+          </div>
+        </div>
+        <div class="FAQsElementClosed" :class="{ FAQsElementOpened: isOpened2 }">
+          We accept major credit cards (Visa, Mastercard, American Express, Discover), PayPal, Apple
+          Pay, Google Pay, and Shop Pay for your convenience.
         </div>
       </div>
-      <div class="FAQsElement center">
-        <h2 class="FAQsElementHeader">Do you offer warranties on your products?</h2>
-        <div class="FAQsElementOpen" @click="isOpened3 = !isOpened3" :class="{ rotate: isOpened3 }">
-          +
+      <!--Third Element-->
+      <div class="FAQsElement">
+        <div class="FAQsElementTop">
+          <h2 class="FAQsElementHeader">Do you offer warranties on your products?</h2>
+          <div
+            class="FAQsElementOpenButton"
+            @click="isOpened3 = !isOpened3"
+            :class="{ rotate: isOpened3 }"
+          >
+            +
+          </div>
+        </div>
+        <div class="FAQsElementClosed" :class="{ FAQsElementOpened: isOpened3 }">
+          Yes! We stand behind the quality of our products. Most items come with a manufacturer's
+          warranty. The specific warranty period and terms can vary depending on the product, so
+          please refer to the product description or contact our customer support for details.
         </div>
       </div>
-      <div class="FAQsElement center">
-        <h2 class="FAQsElementHeader">How long does it take to ship my order?</h2>
-        <div class="FAQsElementOpen" @click="isOpened4 = !isOpened4" :class="{ rotate: isOpened4 }">
-          +
+      <!--Fourth Element-->
+      <div class="FAQsElement">
+        <div class="FAQsElementTop">
+          <h2 class="FAQsElementHeader">How long does it take to ship my order?</h2>
+          <div
+            class="FAQsElementOpenButton"
+            @click="isOpened4 = !isOpened4"
+            :class="{ rotate: isOpened4 }"
+          >
+            +
+          </div>
+        </div>
+        <div class="FAQsElementClosed" :class="{ FAQsElementOpened: isOpened4 }">
+          We strive to get your tech goodies to you as quickly as possible! Shipping times vary
+          depending on your location and the shipping method chosen at checkout. Generally, you can
+          expect your order to arrive within 3-7 business days. You'll receive a shipping
+          confirmation email with tracking information as soon as your order is on its way.
         </div>
       </div>
-      <div class="FAQsElement center">
-        <h2 class="FAQsElementHeader">Do you have a physical store I can visit?</h2>
-        <div class="FAQsElementOpen" @click="isOpened5 = !isOpened5" :class="{ rotate: isOpened5 }">
-          +
+      <!--Fifth Element-->
+      <div class="FAQsElement">
+        <div class="FAQsElementTop">
+          <h2 class="FAQsElementHeader">Do you have a physical store I can visit?</h2>
+          <div
+            class="FAQsElementOpenButton"
+            @click="isOpened5 = !isOpened5"
+            :class="{ rotate: isOpened5 }"
+          >
+            +
+          </div>
         </div>
-        <h4></h4>
+        <div class="FAQsElementClosed" :class="{ FAQsElementOpened: isOpened5 }">
+          Electrohut is an online-only retailer, so we don't have any physical stores at this time.
+          However, we offer a wide range of products, competitive prices, and the convenience of
+          shopping from anywhere at any time.
+        </div>
       </div>
     </div>
   </div>
@@ -89,21 +152,46 @@ const isOpened5 = ref(false)
 }
 .FAQsElement {
   width: 100%;
-  height: fit-content;
-  align-self: space-around;
   border: 1px solid black;
   margin: 3%;
+  padding: 3%;
 }
 .FAQsElementHeader {
-  width: 95%;
+  width: 90%;
 }
-.FAQsElementOpen {
+.FAQsElementTop {
+  height: 50px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.FAQsElementClosed {
+  display: none;
+  height: 0%;
+}
+.FAQsElementOpened {
+  display: block;
+}
+.smoothlyOpen {
+  transition: all 0.3s ease-in;
+  /*animation: open 0.6s ease-out;*/
+}
+/*@keyframes open {
+  from {
+    transform: scaleY(50%);
+  }
+  to {
+    transform: scaleY(100%);
+  }
+}*/
+.FAQsElementOpenButton {
   display: flex;
   justify-content: flex-end;
   font-size: 55px;
   font-weight: 400;
   cursor: pointer;
   user-select: none;
+  transition: transform 0.3s;
 }
 .rotate {
   transform: rotate(-45deg);
